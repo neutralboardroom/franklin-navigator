1a9d3e385e7202b0aa480c539fa04935ebadd6c8
neutralboardroom <reachrgnow@gmail.com>
2026-09-17 23:02:57 -0400

    R1332 load finished profile and reviews layer

--- dist/assets/hf36.js
+++ dist/assets/hf36.js
@@ -160,7 +160,10 @@ ready(()=>{
   if(!body)return;
   if(location.pathname.startsWith('/profiles/')){
     loadCss('/assets/r1330-profile.css?v=frnav1330','franklin-profile-r1330');
-    loadScript('/assets/r1330-profile.js?v=frnav1330','franklin-profile-r1330').catch(()=>{});
+    loadCss('/assets/r1332-profile.css?v=frnav1332','franklin-profile-r1332');
+    loadScript('/assets/r1330-profile.js?v=frnav1330','franklin-profile-r1330')
+      .then(()=>loadScript('/assets/r1332-profile.js?v=frnav1332','franklin-profile-r1332'))
+      .catch(()=>loadScript('/assets/r1332-profile.js?v=frnav1332','franklin-profile-r1332').catch(()=>{}));
   }
   normalizePrimaryNav();
   installCommunityConnectorR1311();
